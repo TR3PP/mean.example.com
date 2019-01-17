76dbfc89712d8aee4b79c64c06db72285a6c93c6
TR3PP <urt3@yahoo.com>
2019-01-17 14:37:49 -0600

    Import mongoose and create a connection to the database.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,6 +12,10 @@ var app = express();
 
 var config = require('./config.dev');
 //console.log(config);
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
+console.log(mongoose);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
