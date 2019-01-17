01793461299f05699c9600d2c1d05d5b2f682671
TR3PP <urt3@yahoo.com>
2019-01-17 14:58:51 -0600

    Implement a route for api/users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
+var apiUsersRouter = require('./routes/api/users');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -27,7 +28,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
+app.use('/api/users', apiUsersRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
