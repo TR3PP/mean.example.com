386bb7c0028896000670d1f9dba158e29509e323
TR3PP <urt3@yahoo.com>
2019-01-18 11:40:09 -0600

    add passport to the users model

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var uniqueValidator = require('mongoose-unique-validator');
-
+var passportLocalMongoose = require('passport-local-mongoose');
 //Create a schema
 var Users = new Schema({
   email: {
@@ -19,10 +19,38 @@ var Users = new Schema({
   admin: {
     type: Boolean,
     default: false
+  },
+  hash: {
+    type: String,
+    required: [
+      true,
+      'There was a problem creating your password'
+    ]
+  },
+  salt: {
+    type: String,
+    required: [
+      true,
+      'There was a problem creating your password'
+    ]
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  modified: {
+    type: Date,
+    default: Date.now
   }
+});
+
+Users.pre('save', function(next){
+  this.modified = new Date().toISOString();
+  next();
 });
 
 //Add unique validation properties to the model
 Users.plugin(uniqueValidator);
 
+User.plugin(passportLocalMongoose);
 module.exports  = mongoose.model('Users', Users);
