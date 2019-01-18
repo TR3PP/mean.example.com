9d3f30248aa3040d74900e8e20e3b4bdc4bf9132
TR3PP <urt3@yahoo.com>
2019-01-18 16:22:46 -0600

    add error/success handling to the login form

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -50,7 +50,13 @@ function postRequest(formId, url){
     xhr.send(JSON.stringify(object));
     xhr.onload = function(){
       let data = JSON.parse(xhr.response);
-      console.log(data);
+      //console.log(data);
+      //~line 52
+      if(data.success===true){
+        window.location.href = '/';
+      }else{
+        document.getElementById('formMsg').style.display='block';
+      }
     }
   });
 }
